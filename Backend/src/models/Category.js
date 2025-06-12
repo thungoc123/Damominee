218e6f2a03a550f66b772b5dcd3acee94a460d8b
Damominee <92897465+thungoc123@users.noreply.github.com>
2025-06-12 20:18:52 +0700

    Delete Backend directory

--- Backend/src/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: String,
-  slug: { type: String, unique: true }
-});
-
-module.exports = mongoose.model('Category', categorySchema);
--- Backend/src/models/Like.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const likeSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Like', likeSchema);

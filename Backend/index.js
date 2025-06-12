8436d65ec4dcf5c1151d72e49a3ec09d9a85f162
thungoc123 <nguyenthunngoc>
2025-06-12 20:13:43 +0700

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,18 @@
 require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const mongoose = require("mongoose")
+const app = require('./src/app');
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Node backend 👋" })
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err))
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`))
